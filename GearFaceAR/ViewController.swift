e3735afc06031c929fa36f8244774d4285bd7f98
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-18 17:52:45 -0500

    Initial Commit

---
+++ GearFaceAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GearFaceAR
+//
+//  Created by Oscar Santos on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
