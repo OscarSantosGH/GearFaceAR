a729b6fd882b2660d46af16b929f4c39597a19a2
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-19 23:12:55 -0500

    added: AVCaptureDevice setting

--- GearFaceAR/MainViewController.swift
+++ GearFaceAR/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 import ARGear
 
 let API_HOST = "https://apis.argear.io/api/v3/"
@@ -17,6 +18,9 @@ class MainViewController: UIViewController {
     
     var argSession: ARGSession?
     
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var consoleLabel: UILabel!
     
@@ -24,6 +28,23 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startARSessions()
+        setupCaptureSession()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        argSession?.run()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer = previewLayer else {
+            consoleLabel.text = "preview layer failed"
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
     }
     
     func startARSessions(){
@@ -45,20 +66,149 @@ class MainViewController: UIViewController {
         }
     }
     
+    
+    
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(.builtInTrueDepthCamera, for: .video, position: .front) else {
+            consoleLabel.text = "VideoCapture Failed"
+            return
+        }
+        
+        
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            consoleLabel.text = "VideoInput Failed"
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            consoleLabel.text = "captureSession addInput Failed"
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        } else {
+            consoleLabel.text = "Invalid Input"
+            return
+        }
+        
+        let captureData = AVCaptureVideoDataOutput()
+        
+        if captureSession.canAddOutput(captureData) {
+            captureSession.addOutput(captureData)
+            captureData.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        } else {
+            consoleLabel.text = "Invalid Input"
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+        cameraView.layer.addSublayer(previewLayer!)
+        
+    }
+    
+    
+    
     @IBAction func runSession(_ sender: Any) {
-        argSession?.run()
+        //argSession?.run()
     }
     
     @IBAction func beautyAction(_ sender: Any) {
+        guard let session = argSession, let contents = session.contents else {
+            return
+        }
+        //contents.setBulge(.NONE)
+        //contents.setDefaultBeauty()
+        consoleLabel.text = "Beauty ON"
+        
+        // Set a Specific Face Beautification Effect Level
+            contents.setBeauty(.faceSlim, value: 0.9)
+                
+            // Set 16 Face Beautification Effects at the Same Time
+//            var beautyValue: [Float] = [
+//                10.0,
+//                90.0,
+//                55.0,
+//                -50.0,
+//                5.0,
+//                -10.0,
+//                0.0,
+//                35.0,
+//                30.0,
+//                -35.0,
+//                0.0,
+//                0.0,
+//                0.0,
+//                50.0,
+//                0.0,
+//                0.0
+//            ]
+//
+//            let beautyValuePointer: UnsafeMutablePointer<Float> = UnsafeMutablePointer(&beautyValue)
+//            contents.setBeautyValues(beautyValuePointer)
     }
     
     @IBAction func filterAction(_ sender: Any) {
+        if let session = argSession, let contents = session.contents {
+            contents.setBulge(.FUN6)
+        }
     }
     
 }
 
 extension MainViewController: ARGSessionDelegate{
     func didUpdate(_ frame: ARGFrame) {
+        guard let pixelBuffer = frame.renderedPixelBuffer else {
+            return
+        }
+        // draw sublayer(CALayer())'s contents
+        //self.previewLayer!.contents = renderedPixelbuffer
         
+        DispatchQueue.main.async {
+        
+            var flipTransform = CGAffineTransform(scaleX: 1, y: 1)
+
+            CATransaction.flush()
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(0)
+            self.previewLayer!.contents = pixelBuffer
+            let angleTransform = CGAffineTransform(rotationAngle: .pi/2)
+            let transform = angleTransform.concatenating(flipTransform)
+            self.previewLayer!.setAffineTransform(transform)
+            self.previewLayer!.frame = CGRect(x: 0, y: 0, width: self.previewLayer!.frame.size.width, height: self.previewLayer!.frame.size.height)
+            CATransaction.commit()
+        }
+        
+    }
+}
+
+extension MainViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    // AVCaptureMetadataOutput Delegate
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        //argSession?.update(metadataObjects, from: connection)
+    }
+}
+
+extension MainViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    // AVCaptureVideoDataOutput Delegate
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        argSession?.update(sampleBuffer, from: connection)
     }
 }
